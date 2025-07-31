ff60308dcb2a4d5ecf0a591d39a43f79169b6110
unknown <jerong766@gmail.com>
2025-07-31 12:37:14 +0900

    feat: header 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./Layout/Header";
+import Footer from "./Layout/Footer";
 
 function App() {
-  return;
-  <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <main> </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
